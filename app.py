7a79742e30d2fe1cbd403485963b35fdc7d4723b
Jimmy Zeng <jzengg@gmail.com>
2022-01-12 20:56:48 -0500

    add db connection

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
 
 app = Flask(__name__)
+
+DB_HOSTNAME = os.getenv('DB_HOSTNAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=30006, debug=True)
+  print(f'mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}')
+  # app.run(host='0.0.0.0', port=30006, debug=True)
+
+engine = create_engine(f'mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}')
