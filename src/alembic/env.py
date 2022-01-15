461b767f5a1de185395d6d1fc845b2bd94f4e7ed
Jimmy Zeng <jzengg@gmail.com>
2022-01-15 15:03:48 -0500

    try to fix env.py

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -6,7 +6,7 @@ import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
-from config import get_engine_url
+from src.config import get_engine_url
 
 from alembic import context
 
