3e79a043e22e322a66613e470d1d6f50924aa4fe
Jimmy Zeng <jzengg@gmail.com>
2022-01-24 19:45:12 -0500

    add cors

--- src/app.py
+++ src/app.py
@@ -3,8 +3,10 @@ from models import Butterfly, Match
 from elo import get_new_rating
 from db import Session
 from sqlalchemy.sql import func
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
